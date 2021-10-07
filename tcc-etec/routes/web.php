607c8d6bea6e4270b12248717b311df8cef34d54
Álef <alefsouzamac1023@gmail.com>
2021-10-07 10:02:03 -0300

    chat

--- tcc-etec/routes/web.php
+++ tcc-etec/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Web\PageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,6 +25,9 @@ Route::get('/', function () {
     ]);
 });
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+// Route::get('/', [PageController::class, 'welcome'])->name('welcome');
+
+Route::group(['middleware'=> ['auth:sanctum', 'verified']], function(){
+    Route::get('/dashboard', [PageController::class, 'dashboard'])->name('dashboard');
+    Route::get('/chat', [PageController::class, 'chat'])->name('chat');
+});
